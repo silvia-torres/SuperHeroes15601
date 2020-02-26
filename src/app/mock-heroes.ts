7f2b45e8f5c06a46da16fa3c57acda7a2e48e343
silvia-torres <silvia.torres.1dv@gmail.com>
2020-02-25 19:48:11 -0600

    commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+export const HEROES: Hero[] = [
+{ id: 11, name: 'Ironman' },
+{ id: 12, name: ' Vision' },
+{ id: 13, name: ' Capitan America' },
+{ id: 14, name: ' Hawkeye' },
+{ id: 15, name: 'Dr Strange' },
+{ id: 16, name: 'Capitana Marvel' },
+{ id: 17, name: 'Black panther' },
+{ id: 18, name: 'Spiderman' },
+{ id: 19, name: 'Hulk ' },
+{ id: 20, name: 'Thor' }
+];
